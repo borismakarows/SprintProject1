d446d7fe87b9fc7d9c4a54106579d559048a1f7c
Berkay <maybedevelopere@gmail.com>
2026-04-17 05:52:38 +0200

    video added

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] float videoDelay = 0.3f;
+    [SerializeField] VideoPlayer startCutsceneVideo;
+    [SerializeField] GameObject cutSceneUI;
+    [SerializeField] Image cutsceneFinalImage;
+
+    void Start()
+    {
+        StartCoroutine(CutsceneFinish());
+    }
+    private IEnumerator CutsceneFinish()
+    {
+        yield return new WaitForSeconds(videoDelay);
+        startCutsceneVideo.gameObject.SetActive(false);
+        cutsceneFinalImage.gameObject.SetActive(true);
+    }
+
+    public void CloseTheLetterUI()
+    {
+        cutSceneUI.SetActive(false);
+    }
+}
